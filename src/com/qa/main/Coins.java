62a82611a0a104c5c8761457371d92b8d5c72516
kidd <ekidd@qa.com>
2021-01-07 18:01:42 +0000

    v2

--- src/com/qa/main/Coins.java
+++ src/com/qa/main/Coins.java
@@ -5,7 +5,7 @@ public class Coins {
 			if(given==cost) {
 				System.out.println("No change.");
 			} else {
-				double received = 0;
+				double difference = given - cost;
 				String owed = "You receive:\n";
 				int a = 0;
 				int b = 0;
@@ -20,105 +20,121 @@ public class Coins {
 				int k = 0;
 				int l = 0;
 				
-				while(cost-a*received>0) {
-					if(cost-(a+1)*50>0) {
+				while(difference-a*50>=0) {
+					if(difference-(a+1)*50>=0) {
 						a++;
 					} else {
-						owed += a + "£50 note(s)\n";
-						cost = cost - a*50;
+						owed += a + " £50 note(s)\n";
+						difference = difference - a*50;
+						break;
 					}
 				}
-				while(cost-b*received>0) {
-					if(cost-(b+1)*20>0) {
+				while(difference-b*20>=0) {
+					if(difference-(b+1)*20>0) {
 						b++;
+						System.out.println("*b");
 					} else {
-						owed += b + "£20 note(s)\n";
-						cost = cost - b*20;
+						owed += b + " £20 note(s)\n";
+						difference = difference - b*20;
+						System.out.println("b");
+						break;
 					}
 						
-			}
-				while(cost-c*received>0) {
-					if(cost-(c+1)*10>0) {
+				}
+				while(difference-c*10>=0) {
+					if(difference-(c+1)*10>0) {
 						c++;
+						System.out.println("*c");
 					} else {
-						owed += c + "£10 note(s)\n";
-						cost = cost - c*10;
+						owed += c +  "£10 note(s)\n";
+						difference = difference - c*10;
+						System.out.println("c");
+						break;
 					}
-		}
-				while(cost-d*received>0) {
-					if(cost-(d+1)*5>0) {
+				}
+				while(difference-d*5>=0) {
+					if(difference-(d+1)*5>0) {
 						d++;
 					} else {
-						owed += d + "£5 note(s)\n";
-						cost = cost - d*5;
-
-}
-				while(cost-e*received>0) {
-						if(cost-(e+1)*2>0) {
-							e++;
+						owed += d + " £5 note(s)\n";
+						difference = difference - d*5;
+						break;
+					}
+				}
+				while(difference-e*2>=0) {
+					if(difference-(e+1)*2>0) {
+						e++;
+					} else {
+						owed += e + " £2 coins(s)\n";
+						difference = difference - e*2;
+						break;
+					}
+				}
+				while(difference-f*1>=0) {
+					if(difference-(f+1)*1>0) {
+						f++;
+					} else {
+						owed += f +  " £1 coins(s)\n";
+						difference = difference - f*1;
+						break;
+					}
+				}
+				while(difference-g*0.05>=0) {
+						if(difference-(g+1)*0.5>0) {
+							g++;
 						} else {
-							owed += e + "£2 coins(s)\n";
-							cost = cost - e*2;
+							owed += g + " 50p coins(s)\n";
+							difference = difference - g*0.5;
+							break;
+						}
+				}
+				while(difference-h*0.2>=0) {
+						if(difference-(h+1)*0.2>0) {
+							h++;
+						} else {
+							owed += h + " 20p coins(s)\n";
+							difference = difference - e*.2;
+							break;
+						}
+				}
+				while(difference-i*0.1>=0) {
+						if(difference-(i+1)*0.1>0) {
+							i++;
+						} else {
+							owed += i + " 10p coins(s)\n";
+							difference = difference - i*0.1;
+							break;
+						}
+				}
+				while(difference-j*0.05>=0) {
+						if(difference-(j+1)*2>0) {
+							j++;
+						} else {
+							owed += j + " 5p coins(s)\n";
+							difference = difference - j*0.05;
+							break;
+						}
+				}
+				while(difference-k*0.02>=0) {
+						if(difference-(k+1)*0.02>0) {
+							k++;
+						} else {
+							owed += k + " 2p coins(s)\n";
+							difference = difference - 2*0.02;
+							break;
+						}
+				}
+				while(difference-l*0.01>=0) {
+						if(difference-(l+1)*0.01>0) {
+							l++;
+						} else {
+							owed += l + " 1p coins(s)\n";
+							difference = difference - l*0.01;
+							break;
+						}
+				}
+				System.out.println(owed);
+				}
+				}
 }
-				while(cost-f*received>0) {
-							if(cost-(f+1)*1>0) {
-								f++;
-							} else {
-								owed += f + "£1 coins(s)\n";
-								cost = cost - f*1;
-}
-				while(cost-e*received>0) {
-								if(cost-(g+1)*0.5>0) {
-									g++;
-								} else {
-									owed += g + "50p coins(s)\n";
-									cost = cost - g*50;
-}
-				while(cost-h*received>0) {
-									if(cost-(h+1)*0.2>0) {
-										h++;
-									} else {
-										owed += h + "20p coins(s)\n";
-										cost = cost - e*.2;
-									}
-				while(cost-i*received>0) {
-										if(cost-(i+1)*0.1>0) {
-											i++;
-										} else {
-											owed += i + "10p coins(s)\n";
-											cost = cost - i*0.1;
-										}
-				while(cost-j*received>0) {
-											if(cost-(j+1)*2>0) {
-												j++;
-											} else {
-												owed += j + "5p coins(s)\n";
-												cost = cost - j*0.05;
-											}
-											while(cost-k*received>0) {
-												if(cost-(k+1)*0.02>0) {
-													k++;
-												} else {
-													owed += k + "2p coins(s)\n";
-													cost = cost - 2*0.02;
-												}
-												while(cost-l*received>0) {
-													if(cost-(l+1)*0.01>0) {
-														l++;
-													} else {
-														owed += l + "£2 coins(s)\n";
-														cost = cost - l*0.01;
-													}
-												}
-											}
-				}
-				}
-				}
-				}
-				}
-				}
-				}
-			System.out.println(owed);
-			}
-		}
-}
+			
--- src/com/qa/main/Runner.java
+++ src/com/qa/main/Runner.java
@@ -2,6 +2,6 @@ package com.qa.main;
 
 public class Runner {
 	public static void main(String[] arg) {
-	Coins.change(20, 21);
+	Coins.change(100, 105);
 	}
 }
